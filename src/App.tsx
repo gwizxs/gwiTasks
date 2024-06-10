ca25a82660f508bfbb2e114185b8f008615221f4
gwizxs <156608329+gwizxs@users.noreply.github.com>
2024-06-10 16:58:15 +0300

    добавил верстку главной страницы

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,84 @@
-import {  observer } from 'mobx-react-lite';
-import './App.css'
+import React, { useState } from 'react';
+import './App.css';
+import {
+  FileOutlined,
+  TeamOutlined,
+  UserOutlined,
+  HomeOutlined,
+} from '@ant-design/icons';
+import type { MenuProps } from 'antd';
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
 import Dashboard from './components/dashboard';
 import Header from './components/header'
+import {  observer } from 'mobx-react-lite';
 
 
 
-const App = () => {
+const { Content, Footer, Sider } = Layout;
+
+type MenuItem = Required<MenuProps>['items'][number];
+
+function getItem(
+  label: React.ReactNode,
+  key: React.Key,
+  icon?: React.ReactNode,
+  children?: MenuItem[],
+): MenuItem {
+  return {
+    key,
+    icon,
+    children,
+    label,
+  } as MenuItem;
+}
+
+const items: MenuItem[] = [
+  getItem('Home', 'sub0', <HomeOutlined />),
+  getItem('Me', 'sub1', <UserOutlined />),
+  getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
+  getItem('About', '9', <FileOutlined />),
+];
+
+const App: React.FC = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+
   return (
     <>
     <Header/>
-    <main>
+    <Layout style={{ minHeight: '100vh' }}>
+      <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
+        <div className="demo-logo-vertical" />
+        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
+      </Sider>
+      <Layout>
+        <Content style={{ margin: '0 16px' }}>
+          <Breadcrumb style={{ margin: '16px 0' }}>
+            <Breadcrumb.Item>User</Breadcrumb.Item>
+          </Breadcrumb>
+          <div
+            style={{
+              padding: 24,
+              minHeight: 360,
+              background: colorBgContainer,
+              borderRadius: borderRadiusLG,
+            }}
+          >
+                <main>
       <Dashboard/>
     </main>
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>
+          gwask ©{new Date().getFullYear()} Created by Ant UED
+        </Footer>
+      </Layout>
+    </Layout>
     </>
   );
-}
+};
 
 // eslint-disable-next-line react-refresh/only-export-components
 export default observer(App);
