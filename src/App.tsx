4275e28e3459910fef845112683cfc027ab51c30
gwizxs <156608329+gwizxs@users.noreply.github.com>
2024-06-11 16:48:01 +0300

    добавил маршрутизатор

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import {
   FileOutlined,
@@ -7,10 +7,12 @@ import {
   HomeOutlined,
 } from '@ant-design/icons';
 import type { MenuProps } from 'antd';
-import { Breadcrumb, Layout, Menu, theme } from 'antd';
+import { Breadcrumb, Layout, Menu, theme, Spin } from 'antd';
 import Dashboard from './components/dashboard';
 import Header from './components/header'
 import {  observer } from 'mobx-react-lite';
+import { Link } from 'react-router-dom';
+
 
 
 
@@ -33,49 +35,75 @@ function getItem(
 }
 
 const items: MenuItem[] = [
-  getItem('Home', 'sub0', <HomeOutlined />),
-  getItem('Me', 'sub1', <UserOutlined />),
-  getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
-  getItem('About', '9', <FileOutlined />),
+  getItem(<Link to="/vite-project/">Home</Link>, 'sub0', <HomeOutlined />),
+  getItem(<Link to="/vite-project/Me">Me</Link>, 'sub1', <UserOutlined />),
+  
+getItem
+('Team', 'sub2', <TeamOutlined />, [
+  getItem(<Link to="/vite-project/Team/1">Team 1</Link>, '6'),
+  getItem(<Link to="/vite-project/Team/2">Team 2</Link>, '8'),
+]),
+getItem(<Link to="/vite-project/About">About</Link>, '9', <FileOutlined />),
 ];
 
 const App: React.FC = () => {
   const [collapsed, setCollapsed] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
   const {
     token: { colorBgContainer, borderRadiusLG },
   } = theme.useToken();
 
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      setTimeout(() => {
+        setIsLoading(false)
+      }, 50);
+    };
+
+    fetchData();
+  }, []);
+  
+
   return (
     <>
-    <Header/>
-    <Layout style={{ minHeight: '100vh' }}>
-      <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
-        <div className="demo-logo-vertical" />
-        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
-      </Sider>
-      <Layout>
-        <Content style={{ margin: '0 16px' }}>
-          <Breadcrumb style={{ margin: '16px 0' }}>
-            <Breadcrumb.Item>User</Breadcrumb.Item>
-          </Breadcrumb>
-          <div
-            style={{
-              padding: 24,
-              minHeight: 360,
-              background: colorBgContainer,
-              borderRadius: borderRadiusLG,
-            }}
-          >
-                <main>
-      <Dashboard/>
-    </main>
-          </div>
-        </Content>
-        <Footer style={{ textAlign: 'center' }}>
-          gwask ©{new Date().getFullYear()} Created by Ant UED
-        </Footer>
-      </Layout>
-    </Layout>
+      {isLoading ? (
+        <Spin tip="Loading" size="large" className='load'>
+          .
+        </Spin>
+      ) : (
+        <>
+          <Header />
+          <Layout style={{ minHeight: '100vh' }}>
+            <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
+              <div className="demo-logo-vertical" />
+              <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
+            </Sider>
+            <Layout>
+              <Content style={{ margin: '0 16px' }}>
+                <Breadcrumb style={{ margin: '16px 0' }}>
+                  <Breadcrumb.Item>User</Breadcrumb.Item>
+                </Breadcrumb>
+                <div
+                  style={{
+                    padding: 24,
+                    minHeight: 360,
+                    background: colorBgContainer,
+                    borderRadius: borderRadiusLG,
+                  }}
+                >
+                  <main>
+                    <Dashboard />
+                  </main>
+                </div>
+              </Content>
+              <Footer style={{ textAlign: 'center' }}>
+                gwask ©{new Date().getFullYear()} Created by Ant UED
+              </Footer>
+            </Layout>
+          </Layout>
+        </>
+      )}
     </>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,28 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import RootStore from './store/index-Store.ts';
+import { createBrowserRouter, RouterProvider} from 'react-router-dom';
+import { Button, Result } from 'antd';
 
 const store = RootStore.create({});
-
+const router = createBrowserRouter([
+  {
+  path: '/vite-project/',
+  element: <App />,
+  errorElement: <Result
+  status="404"
+  title="404"
+  subTitle="Sorry, the page you visited does not exist."
+  extra={<Button type="primary" href='/vite-project/' >Back Home</Button>}
+/>
+  },
+]);
 export const StoreContext = createContext(store)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <StoreContext.Provider value={store}>
-    <App />
+    <RouterProvider router={router} />
     </StoreContext.Provider>
   </React.StrictMode>,
 )
