8dfb2a97a9c97e8d033e252496c1fdac05d2bc1f
gwizxs <gwizxs@gmail.com>
2024-10-14 21:11:23 +0300

    fix: api and add user error handler

--- src/shared/api/interceptors.ts
+++ src/shared/api/interceptors.ts
@@ -1,14 +1,15 @@
-import axios, {type CreateAxiosDefaults} from "axios";
+import axios, { type CreateAxiosDefaults } from "axios";
 import { getAccessToken, removeFromStorage } from "../service/auth-token.service";
 import { errorCatch } from "./errors";
 import { authService } from "../service/auth.service";
+import { Bounce, toast } from "react-toastify";
 
 
 
-const options:CreateAxiosDefaults = {
+const options: CreateAxiosDefaults = {
     baseURL: process.env.REACT_APP_BASE_URL,
     headers: {
-        'Content-Type' : 'application/json'
+        'Content-Type': 'application/json'
     },
     withCredentials: true
 
@@ -18,39 +19,68 @@ const axiosClassic = axios.create(options)
 
 const axiosWithAuth = axios.create(options)
 
-axiosWithAuth.interceptors.request.use(config => {
-    const accessToken = getAccessToken()
 
-    if (config?.headers && accessToken)
-        config.headers.Authorization = `Bearer ${accessToken}`
+axiosWithAuth.interceptors.request.use(
+    (config) => {
+        const accessToken = getAccessToken()
 
-    return config
-})
+        if (config?.headers && accessToken)
+            config.headers.Authorization = `Bearer ${accessToken}`
 
-
+        return config
+    },
+    (error) => { return Promise.reject(error) }
+)
 axiosWithAuth.interceptors.response.use(
     config => config,
     async error => {
-        const originalRequest = error.config
+        const originalRequest = error.config;
 
         if (
-            (error?.response?.status === 401 || 
+            (error?.response?.status === 401 ||
                 errorCatch(error) === 'jwt expired' ||
                 errorCatch(error) === 'jwt must be provided') &&
-                error.config && 
-                !error.config._isRetry
-            ) {
-                originalRequest._isRetry = true
-                try {
-                    await authService.getNewTokens()
-                    return axiosWithAuth.request(originalRequest)
-                } catch (error) {
-                    if (errorCatch(error) === 'jwt expired') removeFromStorage()
+            error.config &&
+            !error.config._isRetry
+        ) {
+            originalRequest._isRetry = true;
+            try {
+                await authService.getNewTokens();
+                return axiosWithAuth.request(originalRequest);
+            } catch (error) {
+                if (errorCatch(error) === 'jwt expired') {
+                    removeFromStorage();
+                    toast.error('Сессия истекла. Пожалуйста, авторизуйтесь снова.', {
+                        position: "top-right",
+                        autoClose: 3000,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: true,
+                        theme: "light",
+                        transition: Bounce
+                    });
                 }
             }
+        }
+        if (error.response && error.response.data && error.response.data.status !== 200) {
+            const message = error.response.data.message || 'Произошла ошибка';
+            toast.error(message, {
+                position: "top-right",
+                autoClose: 3000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                theme: "light",
+                transition: Bounce
+            });
+        }
 
-            throw error
+        return Promise.reject(error); 
     }
-)
+);
 
-export {axiosClassic, axiosWithAuth}
+
+
+export { axiosClassic, axiosWithAuth }
